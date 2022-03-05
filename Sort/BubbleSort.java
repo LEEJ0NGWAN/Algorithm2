21491bd9f1041b40755c54c0015c1c3602c0842d
LEEJ0NGWAN <lastffx@gmail.com>
2022-03-05 23:49:34 +0900

    bubble sort

---
+++ Sort/BubbleSort.java
@@ -0,0 +1,14 @@
+public class BubbleSort{
+
+    public static void sort(int[] arr) {
+
+        for (int i = 0; i < arr.length-1; i++)
+        for (int j = i+1; j < arr.length; j++)
+        if (arr[i] > arr[j]) {
+
+            arr[i] = arr[i] + arr[j];
+            arr[j] = arr[i] - arr[j];
+            arr[i] = arr[i] - arr[j];
+        }
+    }
+}
